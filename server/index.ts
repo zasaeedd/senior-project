07b36afa06f58ffad0482f3598f5e7f7bf83414e
zasaeed <zainabsaeed2004@gmail.com>
2026-02-28 07:42:09 +0300

    added cors config

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,16 @@
 import "dotenv/config";
+import cors from "cors"
 import express from "express";
 import authRoutes from "./routes/auth";
 
 
 const app = express();
+
+// cors configuration to allow requests from frontend
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
 
 // Middleware allows backend to read JSON body
 app.use(express.json());
