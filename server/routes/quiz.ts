563698b36dbea13a665d007887a60fcf77736166
zasaeed <zainabsaeed2004@gmail.com>
2026-03-11 23:54:17 +0300

    quiz creation api

---
+++ server/routes/quiz.ts
@@ -0,0 +1,10 @@
+import express from "express";
+// import { login, getCurrentUser } from "../controllers/authController";
+// import { authenticate } from "../middleware/authMiddleware";
+
+const router = express.Router()
+
+// handle user login and create token
+// router.post("/creation", handler);
+
+export default router
